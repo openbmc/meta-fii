e5b7d7825a4c4f16099af22f9e90431883b91fed
Lancelot Kao <lancelot.kao@fii-usa.com>
2021-01-05 21:38:18 -0600

    meta-fii/meta-kudo: Add kudo flash tool
    1. Support for the Nuvoton JTAG function
    2. Add the phosphor-image package
    
    Tested-by:
    1. Load image on the Kudo BMC board
    2. update the CPLD FW
    3. update the BIOS FW
    
    Signed-off-by: Lancelot Kao <lancelot.kao@fii-usa.com>
    Signed-off-by: Mohaimen alsmarai <Mohaimen.alsamarai@fii-na.com>
    Change-Id: Ia8926146f234775b359c21352fb8b96d8f52c8b1

--- meta-kudo/recipes-phosphor/console/obmc-console/obmc-console-server-setup.sh
+++ meta-kudo/recipes-phosphor/console/obmc-console/obmc-console-server-setup.sh
@@ -17,22 +17,12 @@
 set -euo pipefail
 
 tty="$1"
-uart=0
-
-case "${tty}" in
- "ttyS1") uart=2
- ;;
- "ttyS3") uart=4
- ;;
- *) echo "Invalid tty passed to $0. Exiting!"
-    exit 1;
- ;;
-esac
 
 # Default the host routing through the mux to use the BMC (2)
 # This allows the SoL console in webui, and the ssh port 2200, to work
 # upon startup. If UART transcievers are installed on the header and required,
 # this value should be set to 1
-#/usr/sbin/kudo_uart_mux_ctrl.sh ${uart} 2
+#/usr/sbin/kudo_uart_mux_ctrl.sh ${uart}
+/usr/sbin/kudo_uart_mux_ctrl.sh $1
 
 /usr/sbin/obmc-console-server --config /etc/obmc-console/server.${tty}.conf ${tty}
