417846de40bea54df5d5d50a8096687c848ca10f
Lancelot Kao <lancelot.kao@fii-usa.com>
2021-01-22 19:34:55 -0600

    meta-fii/meta-kudo: Kudo system utilities
    1. Add kudo script files
    
    Tested-by:
    1. Load image on the Kudo BMC board
    
    Signed-off-by: Lancelot Kao <lancelot.kao@fii-usa.com>
    Signed-off-by: Mohaimen alsmarai <Mohaimen.alsamarai@fii-na.com>
    Change-Id: Id90d4f57ebbbb2010be94ec5e96768e63db29c09

--- meta-kudo/recipes-phosphor/fans/phosphor-pid-control/fan-default-speed.sh
+++ meta-kudo/recipes-phosphor/fans/phosphor-pid-control/fan-default-speed.sh
@@ -2,7 +2,7 @@
 
 for i in {1..6};
 do
-    echo 1 > /sys/class/hwmon/hwmon4/pwm${i}_enable
-    echo 155 > /sys/class/hwmon/hwmon4/pwm${i}
+  echo  1 > /sys/devices/platform/ahb/ahb:apb/f0081000.i2c/i2c-1/i2c-18/18-002c/hwmon/*/pwm${i}_enable
+  echo 210 > /sys/devices/platform/ahb/ahb:apb/f0081000.i2c/i2c-1/i2c-18/18-002c/hwmon/*/pwm${i}
 done
 
